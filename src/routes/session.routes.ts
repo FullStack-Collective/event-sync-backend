ec8da5222b0b39ec72c840571dc0f8e67afd81fa
DavFilsDev <miharisoadavidfils@gmail.com>
2026-05-09 07:03:19 +0300

    fix(sessions): remove duplicate path sessions in sessionsRoutes

--- src/routes/session.routes.ts
+++ src/routes/session.routes.ts
@@ -12,7 +12,7 @@ router.get('/:id', sessionController.getOne);
 router.post('/', authMiddleware, requireAdmin, sessionController.create);
 router.put('/:id', authMiddleware, requireAdmin, sessionController.update);
 router.delete('/:id', authMiddleware, requireAdmin, sessionController.delete);
-router.post('/sessions/:sessionId/speakers/:speakerId', authMiddleware,SpeakerController.addSessionToSpeaker);
+router.post('/:sessionId/speakers/:speakerId', authMiddleware,SpeakerController.addSessionToSpeaker);
 
 
 export default router;
